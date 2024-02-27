eefd89099c58260d8871bfd49c32538a21571eb2
N1xx <bikotegame@gmail.com>
2024-02-27 09:35:15 +0300

    +html +db +models +views -Pycharm crash

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date_read = models.DateField()
+
--- web/views.py
+++ web/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def main_view(request):
+    return render(request, 'web/main.html')
+#
+# def add_book(reqwest):
+#     form = AddBook
+#     return render(reqwest, 'web/main.html')
